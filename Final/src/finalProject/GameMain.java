f7d91bbdc2c93d680a8d6335b8f0b4d7de5ba093
wirstrvp <wirstrvp@RHIT-PW0BFLWG.rose-hulman.edu>
2025-05-07 19:28:05 -0400

    vehicle stuff

--- Final/src/finalProject/GameMain.java
+++ Final/src/finalProject/GameMain.java
@@ -5,50 +5,21 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class GameMain {
-//	public static void createGUI() {
-//		JFrame frame = new JFrame("Viewer");
-//		frame.add(new MenuPanels());
-//		frame.setSize(1920, 1080);
-//		frame.pack();
-//		frame.setLocationRelativeTo(null); // 2) center on screen
-//		
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setVisible(true);
-//		
-//		
-//	}
-//	public static void main(String[] args) {
-//		SwingUtilities.invokeLater(() -> createGUI());
-//
-//	}
 	public static void createGUI() {
 		JFrame frame = new JFrame("Viewer");
-<<<<<<< Updated upstream
 		frame.add(new MenuPanels());
-		frame.setSize(1800, 1080);
+		frame.setSize(1920, 1080);
 		frame.pack();
-=======
-		// Uncomment for Lab part 2
-//		frame.setLayout(new java.awt.BorderLayout());
-//		MyCanvas canvas = new MyCanvas();
-//		frame.add(canvas, BorderLayout.CENTER);
-//		ControlPanel control = new ControlPanel(canvas, frame);
-//		frame.add(control, BorderLayout.SOUTH);
-
-		frame.add(new ImagePanel());
-		frame.setSize(600, 200);
-//		frame.pack(); // 1) compute preferred size
->>>>>>> Stashed changes
 		frame.setLocationRelativeTo(null); // 2) center on screen
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		// uncomment for Lab part 2
-		frame.requestFocusInWindow(); // 3) allow frame-level listener see the keys
+		
+		
 	}
-
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> createGUI());
+
 	}
 
 }
--- Final/src/finalProject/Vehicle.java
+++ Final/src/finalProject/Vehicle.java
@@ -1,15 +1,52 @@
 package finalProject;
 
-public class Vehicle {
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Vehicle extends JPanel{
 	private int x, y;
+	private final int WIDTH, HEIGHT;
 	private int velocity;
 	private double angle;
 	
 	public Vehicle(int x, int y) {
-		this.x = x;
+		this.x =x;
 		this.y = y;
-		velocity = 0;
+		WIDTH = 0;
+		HEIGHT = 0;
+		changeVelocity(0);
 		angle = 0;
 	}
-	
+
+	public int getVelocity() {
+		return velocity;
+	}
+
+	public void changeVelocity(int velocity) {
+		this.velocity = velocity;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void changeX(int x) {
+		this.x += x;
+	}
+	public void turnAngle(double degrees) {
+		angle += degrees;
+	}
+	public double getAngle() {
+		return angle;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void changeY(int y) {
+		this.y += y;
+	}
 }
