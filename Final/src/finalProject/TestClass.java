0ead6229b876a78525eb6b07edf6c66c6d576d34
rhit-800197678 <sunq3@rose-hulman.edu>
2025-05-12 14:59:56 -0400

    test if Daniel can commit and push

--- Final/src/finalProject/TestClass.java
+++ Final/src/finalProject/TestClass.java
@@ -3,4 +3,5 @@ package finalProject;
 public class TestClass {
 // this is for testing purpose. 
 // from Michael Branch and committed on Michael's computer. 
+// test if Daniel can commit and push
 }
